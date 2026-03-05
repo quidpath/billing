92dbf11609dec4c5337bb7eb992c5f9365bc0e97
Bett <bethwel.kimutai@strathmore.edu>
2026-03-05 11:09:15 +0300

    Fix(stage): logger issue fix

--- billing_service/settings/prod.py
+++ billing_service/settings/prod.py
@@ -88,7 +88,7 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
-        "console": {"class": "logging.streamHandler"},
+        "console": {"class": "logging.StreamHandler"},
     },
     "root": {
         "handlers": ["console"],
